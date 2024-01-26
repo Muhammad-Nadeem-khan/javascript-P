349bbc930f78fa977f350764712ea541ad763598
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2024-01-26 10:10:04 -0800

    sum of two number

---
+++ Basics/variable.js
@@ -0,0 +1,15 @@
+// Variables let var const//
+
+//var is a function scoped
+
+// let and const is a es6 topic
+
+// Very simple Programe Sum of two Number
+
+let a = 10;
+
+let b = 20;
+
+let c = a+b;
+
+console.log(c)
