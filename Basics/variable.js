03e19e9a76a4afed20eb03752b4433547f41e146
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2024-01-29 01:36:12 -0800

    // Simple if else programe

--- Basics/variable.js
+++ Basics/variable.js
@@ -4,7 +4,7 @@
 
 // let and const is a es6 topic
 
-// Very simple Programe Sum of two Number
+//*************************Very simple Programe Sum of two Number********************************************/
 
 let a = 10;
 
@@ -13,3 +13,15 @@ let b = 20;
 let c = a+b;
 
 console.log(c)
+
+//*****************************************/ if else and switch *********************************************//
+
+const marks = 22;
+
+if(marks < 30){
+    console.log("you are fail")
+}
+else{
+   console.log("you are pass")
+}
+
