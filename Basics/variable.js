9a6a15e4b456fd2abe5833faff401eff7107c331
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2024-02-12 05:28:49 -0800

    add else programe

--- Basics/variable.js
+++ Basics/variable.js
@@ -6,22 +6,36 @@
 
 //*************************Very simple Programe Sum of two Number********************************************/
 
-let a = 10;
+// let a = 10;
 
-let b = 20;
+// let b = 20;
 
-let c = a+b;
+// let c = a+b;
 
-console.log(c)
+// console.log(c)
 
 //*****************************************/ if else and switch *********************************************//
 
-const marks = 22;
+// const marks = 22;
 
-if(marks < 30){
-    console.log("you are fail")
-}
-else{
-   console.log("you are pass")
-}
+// if(marks < 30){
+//     console.log("you are fail")
+// }
+// else{
+//    console.log("you are pass")
+
+
+// const time = 14;
+
+// if(time < 11 && time > 1){
+//     console.log("Good Morning")
+// }
+// else if(time > 11 && time < 15){
+//     console.log("Good after Noon")
+// }
+// else if(time > 15 && time < 18){
+//     console.log("Good Evening")
+// }else{
+//   console.log("Good Nights")
+// }
 
