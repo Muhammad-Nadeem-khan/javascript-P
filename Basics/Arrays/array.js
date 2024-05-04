0ffddaf2be2e835c5e42b461fd26ac24f9306a36
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2024-05-04 01:42:07 -0700

    added new topics

--- Basics/Arrays/array.js
+++ Basics/Arrays/array.js
@@ -65,13 +65,50 @@
 
 
 
-const arr = [28,33,55,666,77,99,11]
+// const arr = [28,33,55,666,77,99,11]
 
-let newarr = arr.filter((arr)=>{
-    return arr<70;
-})
+// let newarr = arr.filter((arr)=>{
+//     return arr<70;
+// })
 
-console.log(newarr)
+// console.log(newarr)
+
+// const marks = [20,30,40,50,60,70,80,90,543,2,79,0,1,6,0,4,444,777]
+
+
+// const sum = marks.reduce((firstvalue,lastvalue)=>{
+//     return firstvalue+lastvalue;
+// })
+// console.log(sum)
+
+
+// const list = [0,3,5,7,2,8,9,50,35]
+
+// const list2 = list.sort((x,y)=>x-y);
+
+// console.log(list2)
+
+// const marks = [30,45,22,77,60,55,80,66,35,90]
+
+// const newmarks = marks
+// .sort((a, b)=> a-b)
+// .filter((n)=> n>50)
+// .map((n)=> n+10)
+// .reduce((a,b)=> a+b)
+// console.log(newmarks)
+
+// and this called channing//
+
+
+const students = ['ali' , 'aslam' , 'tariq' , 'Naveed']
+
+for(let i=0; i<students.length; i++){
+    function printarray(students){
+        console.log(students[i])
+    }
+}
+students.push('farooq')
+printarray(students)
 
 
 
--- Basics/PlayGround/repet.js
+++ Basics/PlayGround/repet.js
@@ -1,16 +1,114 @@
-// const a = 10;
-// const b = 20;
+// const students = [
+//   {
+//     marks: 80,
+//     name: 'Ali',
+//     Gpa: 3.7,
+//     ispass: true,
+//     friends: ['asad', 'aslam', 'akram', 'tariq'],
+//     adress: {
+//       city: 'Lahore',
+//       country: 'Pakistan',
+//       Thesil: 'joharabad'
+//     }
+//   },
+//   {
+//     marks: 40,
+//     name: 'akram',
+//     Gpa: 2.2,
+//     ispass: true,
+//     friends: ['asad1', 'aslam2', 'akram3', 'tariq4'],
+//     adress: {
+//       city: 'kpk',
+//       country: 'Pakistan',
+//       Thesil: 'bajawar'
+//     }
+//   },
+//   {
+//     marks: 70,
+//     name: 'sami',
+//     Gpa: 3.9,
+//     ispass: true,
+//     friends: ['asad5', 'aslam6', 'akram7', 'tariq8'],
+//     adress: {
+//       city: 'Lahore',
+//       country: 'Pakistan',
+//       Thesil: 'bajwar'
+//     }
+//   }
+// ];
 
-// const c = a+b;
+
+// let newstudents = students.map((students)=>{
+//   return {
+//     marks:students.marks*2
+//   }.filter((students)=>{
+//     return { marks:students < 100
+//     }
+//   })
+// })
+// console.log(newstudents)
 
 
-// console.log(c)
+
+// let newStudents = students.map((student) => {
+//   return {
+//     ...student,
+//     marks: student.marks * 10
+//   };
+// });
+
+// console.log(newStudents);
 
 
 
-const time = 14 ;
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].name == 'sami' && students[i].adress.city == 'Lahore') {
+//     console.log('you are pass');
+//   }
+// }
 
- if(time < 11 && time > 13){
-    console.log('Good Morning')
- }
- 
+
+
+const students = [
+  {
+    marks: 80,
+    name: 'Ali',
+    Gpa: 3.7,
+    ispass: true,
+    friends: ['asad', 'aslam', 'akram', 'tariq'],
+    adress: {
+      city: 'Lahore',
+      country: 'Pakistan',
+      Thesil: 'joharabad'
+    }
+  },
+  {
+    marks: 40,
+    name: 'akram',
+    Gpa: 2.2,
+    ispass: true,
+    friends: ['asad1', 'aslam2', 'akram3', 'tariq4'],
+    adress: {
+      city: 'kpk',
+      country: 'Pakistan',
+      Thesil: 'bajawar'
+    }
+  },
+  {
+    marks: 70,
+    name: 'sami',
+    Gpa: 3.9,
+    ispass: true,
+    friends: ['asad5', 'aslam6', 'akram7', 'tariq8'],
+    adress: {
+      city: 'Lahore',
+      country: 'Pakistan',
+      Thesil: 'bajwar'
+    }
+  }
+];
+
+let markadd = students.map((student)=>{
+   return student.marks* 2;
+})
+console.log(markadd);
