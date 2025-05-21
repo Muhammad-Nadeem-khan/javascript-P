66147e6925622a49942f7a1531aa938bac3ee646
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2025-05-21 04:12:32 -0700

    add new files

---
+++ playGround/Basics.js
@@ -0,0 +1,12 @@
+//*********************************************Variables********************************************//
+
+// Spaces in between operators
+// Variabale names must be meaninigfull
+// Variable names must be in camelcase
+// Variable names must be in Snake_Case
+// Variable names must be Unique
+// Variable names must not be started from number
+// Keywords can not be used as variable names
+
+
+
