0a0149a49a6d1d2be7c0501e79872f59090649c0
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2025-06-28 20:16:31 -0700

    added new code

---
+++ condition/if.js
@@ -0,0 +1,244 @@
+//***************************************Conditions*******************************************/
+
+// let age = 5;
+
+// if(age > 18){
+//     console.log('You can drive');
+// }
+// else{
+//     console.log('you can not drive');
+// }
+
+// let temp = 45;
+
+// if(temp > 40){
+//     console.log('ohh its too hot outside');
+// }
+// else{
+//     console.log('its prettey wheather');
+// }
+
+
+
+// let marks = 22;
+
+// if(marks < 30){
+//     console.log('you are fail');
+// }
+// else{
+//     console.log('you are');
+// }
+
+
+// const marks = 22;
+
+// if(marks < 30 && marks > 0){
+//     console.log('you are fail')
+// }
+
+// else if(marks > 33 && marks <  50){
+
+//     console.log('you have b grades');
+// }
+
+//***********************************conditionals or ternary operators********************************/
+
+// const marks = 36;
+
+// (marks > 33 ? console.log('pass') : console.log('fail'))
+
+//  marks = 40;
+// (marks > 33 ? console.log('pass'): console.log('Fail'))
+
+
+// const number = 10;
+
+// switch(number){
+//     case 10 :
+//         console.log('yes it is 10');
+//         break;
+        
+//         case 20 :
+//             console.log('NO its not 10 ');
+
+// }
+
+
+//******************************************Template literals*****************************/
+
+//  firstName = 'Nadeem';
+//  lastName = 'khan';
+
+//  console.log(`firstName{nadeem}`);
+
+
+//*****************************************Loops******************************************/
+
+//*For Loop *, While Loop , Do while Loop* /
+
+
+// for(let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// let i =0;
+
+// while(i<10){
+//     console.log(i);
+//     i++
+// }
+
+// let marks = 22;
+
+// if(marks < 33){
+//     console.log('you are fail');
+// }
+// else{
+//     console.log('you are pass');
+// }
+
+// let percentage = 0;
+
+// if(percentage >= 90 && percentage < 100 ){
+//     console.log('you Got A1 Grade');
+// }
+// else if(percentage >= 80 && percentage < 90 ){
+//     console.log('You Got B Grade');
+// }
+
+// else if(percentage >= 70 && percentage < 80){
+//     console.log('you Got C Grade');
+// }
+
+
+// let score = 90;
+
+// if(score >= 90){
+//     console.log('you have A1 Grade');
+// }
+// else if(score >= 80){
+//     console.log('you have B Grade');
+// }
+// else if(score >= 70){
+//     console.log('you have  C Grade');
+
+// }else{
+//     console.log('you are fail');
+// }
+
+// const temp = 20;
+
+// if(temp > 25){
+//     console.log('The weather is good');
+// }
+// else if(temp < 45){
+//     console.log('its too Hot outside');
+// }
+
+//******************************Operotors in js*******************/
+
+
+// let a = 30;
+// let b = 45;
+
+// console.log('a + b  = ', a+b);
+// console.log('a - b  = ', a-b);
+// console.log('a / b  = ', a/b);
+// console.log('a * b  = ', a*b);
+// console.log('a ** b =', a**b);
+// console.log('a % b =', a%b);
+
+//********************Assignment Operators******************/
+
+// let a = 10;
+
+// a +=5;
+// a -=5;
+// a *=5;
+// a /=5;
+// a %=5;
+// a **=5;
+
+// console.log(a);
+
+
+
+// let number = 7;
+
+// if(number % 2){
+//     console.log('This is even number');
+// }else{
+//     console.log('This odd number');
+// }
+
+// let age = 20; 
+
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+// let number = 50;
+
+// if(number < 30){
+//     console.log('This is greater Number');
+// }else{
+//     console.log('This is minmum number');
+// }
+
+
+
+// let number = -5;
+
+// if(number > 0){
+//     console.log('This is Positive number  ')
+// }else{
+//     console.log('This is Negative number');
+// }
+
+// A for 80+
+
+// B for 60–79
+
+// C for 40–59
+
+// Fail for below 40
+
+
+// let percentage = 90;
+
+// if (percentage >= 90 && percentage <= 100) {
+//     console.log('You Got A1 Grade');
+// } else if (percentage >= 80 && percentage < 90) {
+//     console.log('You Got B Grade');
+// } else if (percentage >= 70 && percentage < 80) {
+//     console.log('You Got C Grade');
+// } else if (percentage >= 60 && percentage < 70) {
+//     console.log('You Got D Grade');
+// } else if (percentage >= 50 && percentage < 60) {
+//     console.log('You Got E Grade');
+// } else {
+//     console.log('Sorry, you have Failed');
+// }
+
+
+let percentage = 30;
+
+if(percentage >= 90 && percentage < 100){
+    console.log('You Got A1 Grade');
+}
+else if(percentage >= 80 && percentage < 90){
+    console.log('You Got B Grade');
+}
+else if(percentage >= 70 && percentage < 80){
+    console.log('You Got C Grade');
+}
+else if(percentage >= 60 && percentage < 70){
+    console.log('you Got D Grade ');
+}
+else if(percentage >= 50 && percentage < 60){
+    console.log('You are Fail');
+}else{
+    console.log('sorr you are fail');
+}
--- playGround/Basics.js
+++ playGround/Basics.js
@@ -9,4 +9,49 @@
 // Keywords can not be used as variable names
 
 
+/********************************************* Variables***************************************/
 
+//* let , var , const *//
+
+//* var is normal css Reassignment: ✅ Yes redecletation yes *//
+
+
+
+
+//****************************************let**************************************************/
+
+//* let Introduced: In ES6 (modern JavaScript)
+
+// Scope: Block-scoped (inside {})
+
+// Hoisting: Yes, but not initialized (accessing before declaration throws an error)
+
+// Reassignment: ✅ Yes
+
+// Redeclaration: ❌ No (in the same scope)
+//  and const is es6 topic *//
+
+
+//*****************************************const*******************************************/
+
+// Introduced: In ES6
+
+// Scope: Block-scoped
+
+// Hoisting: Yes, but not initialized
+
+// Reassignment: ❌ No (cannot be reassigned)
+
+// Redeclaration: ❌ No (in the same scope)
+
+
+//******************************************* simple addition *******************************/
+
+
+// const a = 10;
+
+// const b = 20;
+
+// const c = a+b;
+
+// console.log(c);
