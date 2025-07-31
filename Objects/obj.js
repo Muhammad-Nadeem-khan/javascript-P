6bba6340726b4d1ce84764186abf80d4cecf6510
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2025-07-31 04:27:36 -0700

    added json Topic

--- Objects/obj.js
+++ Objects/obj.js
@@ -45,80 +45,107 @@
 // }
 
 
-const users = [
-    {
-        id: 1,
-        name: "Ali Khan",
-        email: "ali.khan@example.com",
-        age: 25,
-        isActive: true,
-        subscription: {
-            type: "Premium",
-            validTill: "2025-12-31",
-            paymentMethod: "Credit Card"
-        },
-        address: {
-            city: "Lahore",
-            country: "Pakistan",
-            zip: "54000"
-        },
-        loginHistory: [
-            { date: "2025-07-01", status: "Success" },
-            { date: "2025-06-30", status: "Failed" }
-        ],
-        friends: ["Zain", "Farhan", "Imran"]
-    },
-    {
-        id: 2,
-        name: "Sara Ahmed",
-        email: "sara.ahmed@example.com",
-        age: 30,
-        isActive: false,
-        subscription: {
-            type: "Free",
-            validTill: null,
-            paymentMethod: null
-        },
-        address: {
-            city: "Karachi",
-            country: "Pakistan",
-            zip: "75500"
-        },
-        loginHistory: [
-            { date: "2025-07-02", status: "Success" },
-            { date: "2025-06-28", status: "Success" }
-        ],
-        friends: ["Mina", "Sana"]
-    },
-    {
-        id: 3,
-        name: "Bilal Yousaf",
-        email: "bilal.yousaf@example.com",
-        age: 22,
-        isActive: true,
-        subscription: {
-            type: "Standard",
-            validTill: "2025-09-15",
-            paymentMethod: "JazzCash"
-        },
-        address: {
-            city: "Islamabad",
-            country: "Pakistan",
-            zip: "44000"
-        },
-        loginHistory: [
-            { date: "2025-07-03", status: "Failed" },
-            { date: "2025-06-27", status: "Success" }
-        ],
-        friends: []
-    }
-];
+// const users = [
+//     {
+//         id: 1,
+//         name: "Ali Khan",
+//         email: "ali.khan@example.com",
+//         age: 25,
+//         isActive: true,
+//         subscription: {
+//             type: "Premium",
+//             validTill: "2025-12-31",
+//             paymentMethod: "Credit Card"
+//         },
+//         address: {
+//             city: "Lahore",
+//             country: "Pakistan",
+//             zip: "54000"
+//         },
+//         loginHistory: [
+//             { date: "2025-07-01", status: "Success" },
+//             { date: "2025-06-30", status: "Failed" }
+//         ],
+//         friends: ["Zain", "Farhan", "Imran"]
+//     },
+//     {
+//         id: 2,
+//         name: "Sara Ahmed",
+//         email: "sara.ahmed@example.com",
+//         age: 30,
+//         isActive: false,
+//         subscription: {
+//             type: "Free",
+//             validTill: null,
+//             paymentMethod: null
+//         },
+//         address: {
+//             city: "Karachi",
+//             country: "Pakistan",
+//             zip: "75500"
+//         },
+//         loginHistory: [
+//             { date: "2025-07-02", status: "Success" },
+//             { date: "2025-06-28", status: "Success" }
+//         ],
+//         friends: ["Mina", "Sana"]
+//     },
+//     {
+//         id: 3,
+//         name: "Bilal Yousaf",
+//         email: "bilal.yousaf@example.com",
+//         age: 22,
+//         isActive: true,
+//         subscription: {
+//             type: "Standard",
+//             validTill: "2025-09-15",
+//             paymentMethod: "JazzCash"
+//         },
+//         address: {
+//             city: "Islamabad",
+//             country: "Pakistan",
+//             zip: "44000"
+//         },
+//         loginHistory: [
+//             { date: "2025-07-03", status: "Failed" },
+//             { date: "2025-06-27", status: "Success" }
+//         ],
+//         friends: []
+//     }
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i].name === "Ali Khan" && users[i].age === 25) {
+//         console.log('This is present on index', i);
+//     }
+// }
+
+
+
+
+// for(let i = 0; i < users.length; i++){
+//     if (users[i].name === "Ali khan" && users[i].age === 25) {
+//         console.log('This is present on index');
+//     }
+// }
+// for(let i=0; i<students.length; i++){
+//     if(students[i].name == "Ali khan" && students[i].age == 25){
+//         console.log('This is persent on 0 index');
+//     }
+// }
 
 
 
 
 
 
-if(users[1].name == 'Sara Ahmed' && users[1].loginHistory[1]){
-    console.log('good');
-}
+// if(users[1].name == 'Sara Ahmed' && users[1].loginHistory[1]){
+//     console.log('good');
+// }
+
+// console.log(users[2].loginHistory[0].date);
+
+
+
+
+
