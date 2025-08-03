dafcecf135c5027ff0940f931542e59d93d35270
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2025-08-03 01:12:31 -0700

    added new topics

---
+++ Array-Method/array.js
@@ -0,0 +1,17 @@
+// let number = [1,2,33,11,22,7,66]
+
+// const newnumber = number.map((number)=>{
+//     return number*2;
+    
+// })
+// console.log(newnumber);
+
+
+let students = ['Tariq','Naveed','Nadeem','Farooq']
+
+let newStudents = students.map((students)=>{
+   if(students[0] === 'Tariq'){
+    console.log('Yes this is Tariq');
+   }
+   return students
+})
